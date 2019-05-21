5aae6d5d0c80dd08f42d27a727e0771458943ef0
Hyunjin95 <das01063@snu.ac.kr>
2019-05-22 00:28:15 +1000

    Add the first TF file

--- Tutorials_2.0.py
+++ Tutorials_2.0.py
@@ -7,10 +7,10 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer='adam',
@@ -18,5 +18,4 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=5)
-
 model.evaluate(x_test, y_test)
